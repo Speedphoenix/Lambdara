98389761a1d501f3a0a8ff9ca9fc8e6319d23b9c
Pieerre988 <pphilbert94@gmail.com>
2019-04-30 19:18:23 +0200

    Update generic-displays.php

--- src/generic-displays.php
+++ src/generic-displays.php
@@ -1,15 +1,12 @@
 <?php
 function showArticle($what)
 {
-	global $currShopcart;
 
 	echo "<div class='article' id='" . $what["ID"] . "'>";
-echo "pour l'instant vide
+echo  "".$what["nom"] . PHP_EOL . $what["prix"]."
 <form method='post' action=" . $_SERVER['PHP_SELF'] .">
 <input type='number' name='addAmount' min='0'";
 
-if (isset($currShopcart[$what["ID"]]))
-	echo "value='" . $currShopcart[$what["ID"]] . "'";
 
 echo "/>
 <input type='hidden' name='addShopcart' value='" . $what["ID"] . "'>
