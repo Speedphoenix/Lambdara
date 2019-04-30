9d26c4396bf07155aa8461cc764300214ff147f8
Pieerre988 <pphilbert94@gmail.com>
2019-04-30 21:55:43 +0200

    Update generic-displays.php

--- src/generic-displays.php
+++ src/generic-displays.php
@@ -3,15 +3,9 @@ function showArticle($what)
 {
 
 	echo "<div class='article' id='" . $what["ID"] . "'>";
-echo  "".$what["nom"] . PHP_EOL . $what["prix"]."
-<form method='post' action=" . $_SERVER['PHP_SELF'] .">
-<input type='number' name='addAmount' min='0'";
+echo  "<img src=".$what["photo"]."/></br>".$what["nom"]."</br>".$what["prix"]."";
 
 
-echo "/>
-<input type='hidden' name='addShopcart' value='" . $what["ID"] . "'>
-<input type='submit' value='Add to cart'/>
-</form>";
 
 	echo "</div>";
 }
