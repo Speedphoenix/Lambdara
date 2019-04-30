cf14dd7b86e709f918fb228c4c7656496d5b7ff5
Pieerre988 <pphilbert94@gmail.com>
2019-04-30 19:18:55 +0200

    Update config.php

--- src/config.php
+++ src/config.php
@@ -32,20 +32,28 @@ define("DBHOST", "localhost");
 // POUR TESTER. devra proprement être enlevé et remplacé par les données prises de la BD
 $testitems = array(
 	array(
-		"img" => "https://5.imimg.com/data5/XX/BC/MY-71755908/school-book-250x250.jpg",
 		"ID" => 129,
-		"vendeur" => "leo",
+		"photo" => "https://5.imimg.com/data5/XX/BC/MY-71755908/school-book-250x250.jpg",
+		"nom"=>"Les misérables, Victor Hugo",
+		"catégorie"=>"livre",
+		"date_ajout" => time(),
 		"prix" => 45,
-		"date" => time(),
-		"description" => "des livres" . PHP_EOL . "muahaha"
+		"note" => 3,
+		"description" => "des livres" . PHP_EOL . "muahaha",
+		"popularité" => 209,
+		"quantité" => 24
 		),
 	array(
-		"img" => "https://static.fnac-static.com/multimedia/Images/7A/7A/D3/3F/4182906-1505-1540-1/tsp20171106100739/Peluche-mega-ours-beige-1-metre-40-sam-peluche-geante-140-cm.jpg",
 		"ID" => 112,
-		"vendeur" => "leo",
+		"photo" => "https://static.fnac-static.com/multimedia/Images/7A/7A/D3/3F/4182906-1505-1540-1/tsp20171106100739/Peluche-mega-ours-beige-1-metre-40-sam-peluche-geante-140-cm.jpg",
+		"nom"=>"Les misérables, Victor Hugo",
+		"catégorie"=>"livre",
+		"date_ajout" => (time() - 3600 * 24 * 30),
 		"prix" => 57,
-		"date" => (time() - 3600 * 24 * 30),
-		"description" => "une peluche" . PHP_EOL . "muahaha"
+		"note" => 3,
+		"description" => "une peluche" . PHP_EOL . "muahaha",
+		"popularité" => 409,
+		"quantité" => 4
 		),
 );
 
