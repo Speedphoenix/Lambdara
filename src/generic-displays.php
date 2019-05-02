c926e502287412d5433ab054bcca88d725836d36
Speedphoenix <leonardo.jeanteur@gmail.com>
2019-05-02 19:40:24 +0200

    small fix

--- src/generic-displays.php
+++ src/generic-displays.php
@@ -43,7 +43,9 @@ function addToShopcartForm($what)
 		<input type='number' name='addAmount' min='0'";
 
 	if (isset($currShopcart[$what["ID"]]))
-		echo "value='" . $currShopcart[$what["ID"]] . "'/>
+		echo "value='" . $currShopcart[$what["ID"]] . "'";
+		
+		echo "/>
 		<input type='hidden' name='addShopcart' value='" . $what["ID"] . "'>
 		<input type='submit' value='Add to cart'/>
 	</form>";
