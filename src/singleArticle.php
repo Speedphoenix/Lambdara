92a323270c7bd47ff05fb411935d34003fb977c3
Pieerre988 <pphilbert94@gmail.com>
2019-05-02 14:17:43 +0200

    Update singleArticle.php

--- src/singleArticle.php
+++ src/singleArticle.php
@@ -3,84 +3,31 @@
 include_once "config.php";
 include_once "dbFuncs.php";
 
-if (isset($_GET['categ']) && in_array($_GET['categ'], array_keys(POSSIBLECATEGS)))
-	$categ = $_GET['categ'];
-else
-	$categ = DEFAULTCATEG; // 'all'
+//on verifie que l'article est bien passé
+if (empty($_GET['ID']))
+    header("location: category.php"); // ou bien une 404
 
-if (isset($_GET['choix_de_tri']) && in_array($_GET['choix_de_tri'], array_keys(CHOIXDETRI)))
-	$tri = $_GET['choix_de_tri'];
-else
-	$tri = DEFAULTTRI;
+$itemID = $_GET['ID']; //on set notre article avec celui de la BDD correspondant à l'id récupéré
 
-$pageTitle = $categ . " items";
+//on vérifie que la clé de l'article passé existe bien
+if (!itemExists($item["ID"]))
+    header("location: 404.php");
+
+$article = getFromIDs(array($_GET['ID']))[0];
+
+$pageTitle = "article".$itemID;
 
 //ici on chargera tous les items à afficher à partir des BDD
 //tableau de tableaux, chaque sous tableau contient les données de chaque truc
-
-$items = getAllItems($categ); // et le tri si besoin
-
 
 include_once "shopcart-general.php";
 include_once "generic-displays.php";
 
 include "header.php";
 ?>
-<div id="mainContainer">
-<div id="leftpart" class="clearfix">
-    <div class="column search">
-        <input class="field" name="q" type="text" placeholder="Rechercher" /><br>
-        <input class="btn" type="submit" value="Rechercher" /><br><br>
-        <form action='<?php echo $_SERVER['PHP_SELF']; ?>' method="get" >
-            <select name='categ'>
-                <option value="" selected disabled hidden>Categorie</option>
-                <?php
-                    foreach(POSSIBLECATEGS as $key => $val)
-                    {
-                        echo "<option value='$key'>$val</option>"; 
-                    }
-                ?>
-            </select><br><br>
-            <select name="choix_de_tri">
-                <option value="" selected disabled hidden>Trier par</option>
-                <?php
-                    foreach(CHOIXDETRI as $key => $val)
-                    {
-                        echo "<option value='$key'>$val</option>"; 
-                    }
-                ?>
-            </select>
-			<input class="btn" type='submit' value="C'est parti!">
-        </form>
-    </div>
+
+<div>
+    <?php
+    showSingleArticle($article); 
+    ?>   
 </div>
-
-<div id='articleListing'>
-
-	<table>
-
-<?php
-
-foreach ($items as $i)
-{
-	echo "<tr><td>";
-	showSingleArticle($i);
-// tous les trucs à afficher ici
-	echo "</td></tr>";
-}
-?>
-
-	</table>
-</div><!--
-<div id="upperpart" class="clearfix">
-    <div  class="column search">
-        <input class="field" name="q" type="text" placeholder="Rechercher" /><br>
-        <input class="btn" type="submit" value="Rechercher" />
-    </div>
-</div>
--->
-</div>
-<?php
-include "footer.php";
-?>
-
