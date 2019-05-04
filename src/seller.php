4e013ee5fcd1b54baa05f79f3e4a70aaabc29dce
lazovchik <lazovchik@gmail.com>
2019-05-04 18:53:39 +0200

    Seller functional

--- src/seller.php
+++ src/seller.php
@@ -5,11 +5,41 @@ include_once "genericFuncs.php";
 include_once "dbFuncs.php";
 include_once "shopcart-general.php";
 
+
+
 if (empty($_SESSION['username']))
 {
 	$_SESSION['previouspage'] = $_SERVER['PHP_SELF'];
 	header("location: login.php");
-}/*
+}
+
+function updateUser()
+{
+    global $errormsg;
+	$result = true;
+    $values_to_change=array();
+    if(!empty($_POST['email']))
+        $values_to_change["email"] = $_POST['email'];
+    if(!empty($_POST['surname']) && !empty($_POST['name']))
+        $values_to_change["nom_complet"] = $_POST['surname']." ".$_POST['name'];
+    if(!empty($_POST['fileToUpload1']))
+        $values_to_change["img_couverture"] = $_POST['fileToUpload1'];
+    if(!empty($_POST['fileToUpload2']))
+        $values_to_change["img_profil"] = $_POST['fileToUpload2'];
+    //var_dump($values_to_change);
+    if(!empty($values_to_change))     
+		$result = updateInDb('users', $values_to_change, "username='" . $_SESSION['username'] . "'");
+	
+
+	return ($result === true);
+}
+
+if(isset($_POST["submit"]))
+    updateUser();
+
+
+
+/*
 if (USERSTATUSES[getUserInfo($_SESSION['username'], 'statut')] !== 'seller')
 	header("location: category.php");
 */
@@ -53,13 +83,13 @@ include "header.php";
 ?>
     <div class='clearfix'>
 		<div class='vendeur'>
-			<form action="<?php echo $_SERVER['PHP_SELF'] ?>" enctype="multipart/form-data">
+			<form action="seller.php" method="post">
                 <table class="tab">
                     <tr>
                         <?php
                         echo "<th class='bkg' colspan='3' style='background-image: url($bckimg);'>";
                         ?>
-                            <input class="upload_btn" type="file" name="fileToUpload" id="fileToUpload" required/>
+                            <input class="upload_btn" type="file" name="fileToUpload" id="fileToUpload" />
                             <label style="margin:auto; margin-top:150px; margin-left:75%; width:23%;"  for="fileToUpload">Changer photo de couverture</label>
                         </th>
                     </tr>
@@ -68,7 +98,7 @@ include "header.php";
                             echo"<td class='prof' style='width:30%; background-image: url($profimg);'>";
                             /*echo"<div  style='width:auto; margin:auto; margin-top:10%; margin-left:400px;'>$nom_compl</div> ";*/
                         ?>
-                            <input class="upload_btn" type="file" name="fileToUpload2" id="fileToUpload2" required/>
+                            <input class="upload_btn" type="file" name="fileToUpload2" id="fileToUpload2" />
                             <label style="margin:auto; margin-top:270px; margin-right:100%; margin-left:13px; width:250px;"  for="fileToUpload2">Changer photo de profile</label>
                         </td>
                         <td style='width:30%;'>
@@ -76,8 +106,13 @@ include "header.php";
                                 echo"<div style='width:80%; margin:auto; margin-bottom:100%; margin-top:10%; margin-left:20px;'><tit>Utilisateur:</tit><br><util>$nom_compl</util></div> ";
                             ?>
                         </td>
-                        <td style='width:40%;'>
-                            
+                        <td style='width:40%; vertical-align:top;'>
+                            <br><br>
+                            <span>Changer les informations:</span><br><br>
+                            <input class="field" type='text' name='surname' placeholder="Nom:" /><br><br>
+                            <input class="field" type='text' name='name' placeholder="Prenom:" /><br><br>
+                            <input class="field" type='text' name='email' placeholder="E-mail:" /><br><br>
+                            <input class="vend_btn" type='submit' name="submit" value="Enregistrer les changements">
                         </td>
                     </tr>
                 </table>
