5a9966ffe843701bd695caeed22ee7a0407c4e44
lazovchik <lazovchik@gmail.com>
2019-05-04 17:06:51 +0200

    updates on seller.php

--- src/seller.php
+++ src/seller.php
@@ -5,12 +5,12 @@ include_once "genericFuncs.php";
 include_once "dbFuncs.php";
 include_once "shopcart-general.php";
 
-/*if (empty($_SESSION['username']))
+if (empty($_SESSION['username']))
 {
 	$_SESSION['previouspage'] = $_SERVER['PHP_SELF'];
 	header("location: login.php");
-}
-if (USERSTATUSES[getUserInfo($_SESSION['username'], 'statut')] !== 'buyer')
+}/*
+if (USERSTATUSES[getUserInfo($_SESSION['username'], 'statut')] !== 'seller')
 	header("location: category.php");
 */
 $nom_compl = getUserInfo($_SESSION['username'], 'nom_complet');
@@ -57,7 +57,7 @@ include "header.php";
                 <table class="tab">
                     <tr>
                         <?php
-                        echo "<th class='bkg' style='background-image: url($bckimg);'>";
+                        echo "<th class='bkg' colspan='3' style='background-image: url($bckimg);'>";
                         ?>
                             <input class="upload_btn" type="file" name="fileToUpload" id="fileToUpload" required/>
                             <label style="margin:auto; margin-top:150px; margin-left:75%; width:23%;"  for="fileToUpload">Changer photo de couverture</label>
@@ -65,10 +65,19 @@ include "header.php";
                     </tr>
                     <tr>
                         <?php
-                            echo"<td class='prof' style='background-image: url($profimg);'>";
+                            echo"<td class='prof' style='width:30%; background-image: url($profimg);'>";
+                            /*echo"<div  style='width:auto; margin:auto; margin-top:10%; margin-left:400px;'>$nom_compl</div> ";*/
                         ?>
                             <input class="upload_btn" type="file" name="fileToUpload2" id="fileToUpload2" required/>
-                            <label style="margin:auto; margin-top:300px; margin-right:100%; margin-left:13px; width:250px;"  for="fileToUpload2">Changer photo de profile</label>
+                            <label style="margin:auto; margin-top:270px; margin-right:100%; margin-left:13px; width:250px;"  for="fileToUpload2">Changer photo de profile</label>
+                        </td>
+                        <td style='width:30%;'>
+                            <?php
+                                echo"<div style='width:80%; margin:auto; margin-bottom:100%; margin-top:10%; margin-left:20px;'><tit>Utilisateur:</tit><br><util>$nom_compl</util></div> ";
+                            ?>
+                        </td>
+                        <td style='width:40%;'>
+                            
                         </td>
                     </tr>
                 </table>
