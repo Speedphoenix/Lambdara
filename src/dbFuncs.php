8aa769ec7c45ef0b9e94c4471530e2bb75fe52c5
Pieerre988 <pphilbert94@gmail.com>
2019-05-05 10:30:50 +0200

    Update dbFuncs.php

--- src/dbFuncs.php
+++ src/dbFuncs.php
@@ -42,16 +42,21 @@ function orderByTri($tri = "prix-up")
 	return $rep;
 }
 
-function getAllItems($categ = DEFAULTCATEG, $tri = DEFAULTTRI)
+function getAllItems($filtrer, $tri = DEFAULTTRI)
 {
 	$rep = array();
-	$query = "SELECT * FROM Articles";
-	if ($categ !== 'all')
-		$query .= " WHERE categorie='$categ'";
+	$query = "SELECT * FROM Articles WHERE";
+	if ($filtrer[0] !== 'all')
+		$query .= " categorie='$filtrer[0]' AND";
+
+	if ($filtrer[1] !== 'ttt')
+		$query .= " date_ajout> ADDDATE(NOW(), INTERVAL -'$filtrer[1]' MONTH) AND";
+
+	$query .= " prix<'$filtrer[2]'";
 
 	$query .= orderByTri($tri);
 	$query .= ';';
-
+	var_dump($query);
 
 	$conn = connectDB('central');
 	
